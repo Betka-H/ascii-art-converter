60822ab6b7d4e1763a01eb3272301321e8126df5
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-16 19:12:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ fileInput.onchange = (e) => {
     reader.readAsDataURL(file);
 };
 
-const grayRamp ='G%Xůo+=:-.';
+const grayRamp ='GK%Hůou+=:-.';
 const rampLength = grayRamp.length;
 
 const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
