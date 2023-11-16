24fc75233695691bbec9c875708229d2bdfe8af2
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-16 19:59:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const convertToGrayScales = (context, width, height) => {
     return grayScales;
 };
 
-const MAXIMUM_WIDTH = 64;
+const MAXIMUM_WIDTH = 666;
 const MAXIMUM_HEIGHT = 1000;
 const clampDimensions = (width, height) => {
     const rectifiedWidth = Math.floor(getFontRatio() * width);
