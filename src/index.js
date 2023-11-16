4560b3f8f6a50bd361b6d9351090aab80d8496bf
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-11-16 17:17:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ fileInput.onchange = (e) => {
     reader.readAsDataURL(file);
 };
 
-const grayRamp ='=ěščřžýáíé¨\`qwertzuiopasdfghjklů_yxcvbnm,.-+23456789%ˇ'\§\":?!/ĚŠČŘŽÝÁÍÉQWERTZUIOPASDFGHJKLYXCVBNM';
+const grayRamp ='=ěščřžýáíé¨\`qwertzuiopasdfghjklů_yxcvbnm,.-+23456789%ˇ\'§":?!/ĚŠČŘŽÝÁÍÉQWERTZUIOPASDFGHJKLYXCVBNM';
 const rampLength = grayRamp.length;
 
 const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
